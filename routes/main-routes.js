67646cc5a2b8c356f5fe26c7443cbad3f5c501c3
Francois Polo <andasan@gmail.com>
2020-05-21 11:29:53 -0700

    init commit

---
+++ routes/main-routes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fs = require('fs');
+
+const router = express.Router();
+
+const data = [];
+
+router.get('/', (req,res,next) => {
+    res.render('index', { notes: data });
+});
+
+router.use('/remove/:id', (req,res,next)=>{
+    const id = req.params.id;
+    const index = data.findIndex(d => d.id == id);
+    data.splice(index,1);
+    res.status(302).redirect('/');
+});
+
+router.post('/note', (req,res,next) => {
+    data.push({
+        id: Math.random(),
+        note: req.body.note
+    });
+    fs.writeFile('notes.json', JSON.stringify(data, null, 2), ()=> {
+        res.status(302).redirect('/');
+    });
+});
+
+module.exports = router;
